c29af479bddf7af7cbc4100e9ed0a524e50ed5f5
Ebere Okezie <ebereokezie@gmail.com>
2022-04-13 15:26:35 -0500

    commit tree fcb39236ef22ee28021f4733c81e26e0f61d102f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const guestNames = ["Guadalupe", "Ollie", "Aki"]
+let newCard = []
+function writeCards(names, occasion){
+    for(let i=0; i< guestNames.length; i++){
+        newCard.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return newCard
+}
+
+function countDown(){
+    let count = 10
+    while(count >= 0){
+        console.log(count--)
+        ;
+    }
+}
